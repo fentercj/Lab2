befc1fd64b9b90a9ccd92524d8228937f81e9578
fentercj <fentercj@mail.uc.edu>
2021-01-23 21:31:23 +0000

    added variables and PrintLines for variables

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("M&M Color Counts");
+  int total = 605;
+  double blue = .24 * total;
+    System.out.println("blue: " + blue);
+  double brown = .13 * total;
+    System.out.println("brown: " + brown);
+  double green = .16 * total;
+    System.out.println("green: " + green);
+  double orange = .20 * total;
+    System.out.println("orange: " + orange);
+  double red = .13 * total;
+    System.out.println("red: " + red);
+  double yellow = .14 * total;
+    System.out.println("yellow: " + yellow);
   }
-}
+};
